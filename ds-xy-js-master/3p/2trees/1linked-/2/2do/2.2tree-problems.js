eb68694df949cd4124d5d8fe22b77d803339ee47
humberto312 <102559886+humberto312@users.noreply.github.com>
2022-05-20 23:05:36 -0500

    Update 2.2tree-problems.js

--- ds-xy-js-master/3p/2trees/1linked-/2/2do/2.2tree-problems.js
+++ ds-xy-js-master/3p/2trees/1linked-/2/2do/2.2tree-problems.js
@@ -1,27 +1,40 @@
-/** INSTRUCTIONS
- * 
- * create one file by program
- * implement a method, function, non-function solution
- * solve each as requested
- * may use code from previous exercises
- * 
- * 
- * PROBLEM 1
- * inorder(n)
- *
- * traverses a tree in INORDER
- * @arg {tree} n - tree
- *
- * @example
- * inorder(root)
- * 
- * PROBLEM 2
- * posorder(n)
- *
- * traverses a tree in POSORDER
- * @arg {tree} n - tree
- *
- * @example
- * posorder(root)
- * 
- */
+function Node(o){
+    this.d=o//bject<->data
+    this.l=null//eft
+    this.r=null//ight
+  }
+  function preorder(n){
+    if (n!==null){
+      console.log(n.d)
+      preorder(n.l)
+      preorder(n.r)
+    }
+  }
+  function inorder(n){
+    if (n!==null){
+      inorder(n.l)
+      console.log(n.d)
+      inorder(n.r)
+    }
+  }
+  function posorder(n){
+    if (n!==null){
+      posorder(n.l)
+      posorder(n.r)
+      console.log(n.d)
+    }
+  }
+  let r = new Node (55) 
+  r.d = 56
+  let o1 = new Node (56)
+  let o2 = new Node (14)
+  let o3 = new Node (81)
+  let o4 = new Node (13)
+  let o5 = new Node (18)
+  let o6 = new Node (24)
+  r.l = o1
+  r.r = o2
+  r.w = o3
+  r.x = o4
+  r.y = o5
+  r.z = o6
