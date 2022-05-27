0b4972fe38631e87119123a179dcd0192297b338
humberto312 <102559886+humberto312@users.noreply.github.com>
2022-05-27 12:54:43 -0500

    Update 3.1graph-problems.js

--- ds-xy-js-master/3p/3graphs/2do/3.1graph-problems.js
+++ ds-xy-js-master/3p/3graphs/2do/3.1graph-problems.js
@@ -10,3 +10,39 @@
  *
  * write it and make sure it runs
  */
+function Graph(v) {
+   this.vert = v;
+   this.edges = 0;
+   this.adj = [];
+   for (var i = 0; i < this.vert; i++) {
+      this.adj[i] = [];
+      this.adj[i].push("");
+   }
+   this.addEdge = addEdge;
+   this.showGraph = showGraph;
+}
+
+
+function addEdge(v,w) {
+   this.adj[v].push(w);
+   this.adj[w].push(v);
+   this.edges++;
+}
+
+function showGraph() {
+   for (var i = 0; i < this.vert; ++i) {
+      console.log( "<br>" + i + " -> ");
+      for (var j = 0; j < this.vert; ++j) {
+         if (this.adj[i][j] != undefined)
+           console.log(this.adj[i][j] + ' ');
+      }
+      console.log("");
+   }
+}
+  
+g = new Graph(5);
+g.addEdge(0,1);
+g.addEdge(0,2);
+g.addEdge(1,3);
+g.addEdge(2,4);
+g.showGraph();
