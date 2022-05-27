d5559af9b502e110c3ed8822a3e16a9e6d342463
humberto312 <102559886+humberto312@users.noreply.github.com>
2022-05-26 23:11:44 -0500

    Update 1.0linkedlist-problems.js

--- ds-xy-js-master/3p/1linkedlist/0/2do/1.0linkedlist-problems.js
+++ ds-xy-js-master/3p/1linkedlist/0/2do/1.0linkedlist-problems.js
@@ -7,3 +7,10 @@
  *
  * this section has NOT exercises
  */
+let vec = [];
+for(i=0; i<100000;i++){
+  vec[i]=Math.floor(Math.random()*100000+1)
+}
+delete vec[4];
+  vec.splice(3, 5, "new");
+  console.log(vec);
