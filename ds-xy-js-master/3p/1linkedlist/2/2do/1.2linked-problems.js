96221f9f5002b389ce9a2706146288c7fe6c50fb
humberto312 <102559886+humberto312@users.noreply.github.com>
2022-05-27 13:08:55 -0500

    Update 1.2linked-problems.js

--- ds-xy-js-master/3p/1linkedlist/2/2do/1.2linked-problems.js
+++ ds-xy-js-master/3p/1linkedlist/2/2do/1.2linked-problems.js
@@ -20,3 +20,23 @@
  * @see {@link https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array}
  * @see {@link https://jsdoc.app}
  */
+ function Node(data) {//constructor
+     this.data = data
+     this.next = null
+  }
+  
+  
+   let r=new Node(0)//root
+   let p=new Node(1)//previous
+   r.next=p//link
+  	
+  
+  for (let i=0;i<100;i++){
+    	let random = Math.floor(Math.random() * 1000 + 1);
+      let n = new Node("name" + random)//next
+      p.next = n//link
+      p=n
+    	
+  }
+
+	console.log(r)
